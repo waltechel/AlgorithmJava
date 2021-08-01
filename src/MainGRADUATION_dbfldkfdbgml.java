791695b2b44c1a4bf20e3d33fff0492b32069df1
knecht <ldj910505@naver.com>
2021-08-01 23:13:54 +0900

    <code> naming rule에 주의하여 소스를 올려주세요.
    * Main.java, Solution.java가 올라가지 않습니다.
    * 소스를 올리는 양식은 Main문제이름_유저아이디 로 올려주십시오.

---
+++ src/MainGRADUATION_dbfldkfdbgml.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainGRADUATION_dbfldkfdbgml {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		System.out.println("hello world!");		
+		
+		br.close();
+		bw.close();
+		
+	}
+
+}
