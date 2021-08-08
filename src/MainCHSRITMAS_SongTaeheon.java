1bb168a60a5e4cf51e3c1b8c691e801433fad153
taeheon.song <taeheon.song@linecorp.com>
2021-08-08 18:16:36 +0900

    [CHRISTMAS] wrong answer (try to follow book)

--- src/MainCHSRITMAS_SongTaeheon.java
+++ src/MainCHSRITMAS_SongTaeheon.java
@@ -10,7 +10,7 @@ public class MainCHSRITMAS_SongTaeheon {
     static int boxCount, childCount;
     static int[] box;
 
-    static int[] accBox;
+    static int[] accBoxRemainder; // 사실은 나머지만 저장
 
     static int lastSelectedBox;
     static boolean[] possibleSubsetWithEnd; // 0 ~ index까지만 박스를 선택할 때, 선택가능한 subset
@@ -25,65 +25,77 @@ public class MainCHSRITMAS_SongTaeheon {
             init();
             input();
 
-            solveProblem();
+            makeAccBox();
+
+            int result1 = solveProblem1();
+            int result2 = solveProblem2();
+            System.out.println(result1 + " " + result2);
         }
     }
 
-
-    private static void solveProblem() {
-
-        int result1 = 0;
-        int result2 = 0;
-
-        // accumulate box 계산
-        accBox[0] = box[0];
-        for (int i = 1; i < boxCount; i++) {
-            accBox[i] = accBox[i-1] + box[i];
+    private static void makeAccBox() {
+        // accumulate box 의 나머지
+        accBoxRemainder[0] = 0;
+        for (int i = 1; i <= boxCount; i++) {
+            accBoxRemainder[i] = (accBoxRemainder[i - 1] + box[i]) % childCount;
         }
-
-        // box start ~ end 선택
-        for (int end = 0; end < boxCount; end++) {
-
-            for (int start = end; start >= 0; start--) { // 0 ~ end
-
-                int startMinusOne = start -1;
-
-                // start ~ end까지의 sumOfDull을 찾는다.
-                int accBoxOfBeforeStart = 0;
-                if (startMinusOne >= 0) {
-                    accBoxOfBeforeStart = accBox[startMinusOne];
-                }
-
-                int sumOfDull = accBox[end] - accBoxOfBeforeStart; // accBox[end] - accBox[start - 1]
-
-                // 선택 가능한지 확인한다.
-                if (sumOfDull % childCount == 0) {
-
-//                    System.out.println(start + "~" + end + " : sumOfDull - " + sumOfDull);
-                    result1 = (result1 + 1) % resultDivisor;
-
-                    // lastSelectedBox가 start보다 작으면 선택 가능! -> 2번째 질문을 위해!
-                    if (lastSelectedBox < start) {
-                        possibleSubsetWithEnd[end] = true;
-                        lastSelectedBox = end;
-                    }
-                }
-            }
-        }
-
-        // 2번 째 질문의 답을 찾는다.
-        for (int end = 0; end < boxCount; end++) {
-            if (possibleSubsetWithEnd[end]) {
-                result2 += 1;
-            }
-        }
-
-        System.out.println(result1 + " " + result2);
     }
+
+    private static int solveProblem1() {
+
+        int result = 0;
+
+        //나머지 별 개수를 구한다.
+        int[] count = new int[childCount];
+        for (int i = 0; i < boxCount + 1; i++) { // i = 0인 경우는
+            count[accBoxRemainder[i]]++;
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > 1) {
+                // start - 1까지의 합과 end까지의 합을 뺐을 때, 나머지가 나눠떨지면 선택 가능. -> start-1까지의 합의 나머지와 end까지의 합의 나머지가 같으면 가능!
+                // 같은게 2개인 경우는 항상 1가지 경우만 나옴.
+                // 같은게 3개 이상인 경우는 nC2 (n개 중에 2개를 선택하는 경우와 같음.)
+                result = (result + (count[i] * (count[i] - 1)) / 2) % resultDivisor;
+            }
+        }
+
+        return result;
+    }
+
+
+    private static int solveProblem2() {
+
+        int[] subsetCountUntil = new int[boxCount + 1];
+        int[] prevRemainderPosition = new int[childCount + 1];
+        boolean[] prevRemainderExist = new boolean[childCount + 1];
+
+        for (int until = 0; until < boxCount + 1; until++) {
+
+            // until까지만 봤을 때, 겹치지 않는 subset 개수 찾기
+            if (until == 0) {
+                subsetCountUntil[until] = 0;
+            } else {
+                subsetCountUntil[until] = subsetCountUntil[until - 1];
+            }
+
+            // accBoxRemainder의 값이 같은 걸 찾는다. (그 위치 + 1 또는 그냥 그대로)
+            if (prevRemainderExist[accBoxRemainder[until]]) {
+                int loc = prevRemainderPosition[accBoxRemainder[until]];
+                subsetCountUntil[until] = Math.max(subsetCountUntil[until], subsetCountUntil[loc] + 1);
+            }
+
+            prevRemainderPosition[accBoxRemainder[until]] = until; // remainder의 위치 기억
+            prevRemainderExist[accBoxRemainder[until]] = true;
+        }
+
+        return subsetCountUntil[boxCount];
+    }
+
 
     private static void printAcc() {
         for (int i = 0 ; i < boxCount; i++) {
-            System.out.print(accBox[i] + " ");
+            System.out.print(accBoxRemainder[i] + " ");
         }
         System.out.println();
     }
@@ -100,16 +112,26 @@ public class MainCHSRITMAS_SongTaeheon {
         childCount = Integer.parseInt(stNumber.nextToken()); // K
 
         StringTokenizer stBox = new StringTokenizer(br.readLine());
-        for (int i = 0; i < boxCount; i++) {
+
+        box[0] = 0;
+        for (int i = 1; i <= boxCount; i++) {
             box[i] = Integer.parseInt(stBox.nextToken());
         }
     }
 
     private static void init() {
-        box = new int[100000];
-        accBox = new int[100000];
+        box = new int[100001];
+        accBoxRemainder = new int[100001];
         lastSelectedBox = -1;
-        possibleSubsetWithEnd = new boolean[100000];
+        possibleSubsetWithEnd = new boolean[100001];
     }
+
+    private static void printArr(int[] count) {
+        for (int i = 0; i < count.length; i++) {
+            System.out.print(count[i] + " ");
+        }
+        System.out.println();
+    }
+
 
 }
