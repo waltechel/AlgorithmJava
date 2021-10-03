ad521ea8c2313213a1aeda00181580f5be7962aa
SongTeaheon <sth534@naver.com>
2021-10-03 21:22:47 +0900

    [MEETINGROOM] FAIL 20211003

--- src/MainMEETINGROOM_SongTaeheon.java
+++ src/MainMEETINGROOM_SongTaeheon.java
@@ -33,94 +33,23 @@ public class MainMEETINGROOM_SongTaeheon {
             System.out.println("IMPOSSIBLE");
         } else {
             System.out.println("POSSIBLE");
-            for (int i: result) {
-                if (i == 1) {
+            for (int i = 0; i < result.length; i++) {
+                if (result[i] == 1) {
                     System.out.println(input[i][0] + " " + input[i][1]);
                 }
             }
         }
     }
 
-    private static int[] solve2SAT() {
-        int n = graph.size();
-
-        List<Integer> label = tarjanSCC();
-
-        // check possible
-        for (int i = 0; i < 2 * N; i++) {
-            if (Objects.equals(label.get(i), label.get(i + 1))) {
-                return new int[0];
+    private static void printGraph() {
+        System.out.println("size : " + graph.size());
+        for (int i = 0; i < graph.size(); i++) {
+            System.out.print(i + " : ");
+            for (int j = 0; j < graph.get(i).size(); j++) {
+                System.out.print(graph.get(i).get(j) + " ");
             }
+            System.out.println();
         }
-
-        int[] value = new int[2 * n];
-        Arrays.fill(value, -1);
-
-        List<Pair> order = new ArrayList<>();
-        for (int i = 0; i < 2 * N; i++) {
-            order.add(new Pair(label.get(i) * -1, i));
-        }
-
-        order.sort(Comparator.comparing(Pair::getFirst));
-
-        for (int i = 0; i < 2 *n; i++) {
-            int vertex = order.get(i).second;
-            int variable = vertex / 2;
-            int isFalse = vertex % 2 == 0 ? 0 : 1;
-
-            if (value[variable] != -1) continue;
-
-            value[variable] = isFalse;
-        }
-        return value;
-
-    }
-
-
-    static List<List<Integer>> adj;
-    static int[] sccId;
-    static int[] discovered;
-    static Stack<Integer> st;
-    static int sccCounter, vertexCounter;
-
-    private static List<Integer> tarjanSCC() {
-        init();
-
-        for (int i = 0; i < adj.size(); i++) {
-            if (discovered[i] == -1) scc(i);
-        }
-        return Arrays.stream(sccId).boxed().collect(toList());
-    }
-
-    private static int scc(int here) {
-
-        vertexCounter += 1;
-        int ret = vertexCounter;
-        discovered[here] = vertexCounter;
-        st.push(here);
-
-        for (int i = 0; i < adj.get(here).size(); i++) {
-            int there = adj.get(here).get(i);
-
-            if (discovered[there] == -1) ret = Math.min(ret, scc(there));
-            else if (sccId[there] == -1) ret = Math.min(ret, discovered[there]);
-        }
-
-        if (ret == discovered[here]) {
-            while (true) {
-                int t = st.pop();
-                sccId[t] = sccCounter;
-                if (t == here) break;
-            }
-            sccCounter += 1;
-        }
-        return ret;
-    }
-
-    private static void init() {
-        adj = new ArrayList<>();
-        sccId = new int[2 * N];
-        discovered = new int[2 * N];
     }
 
     private static void makeGraph() {
@@ -140,11 +69,100 @@ public class MainMEETINGROOM_SongTaeheon {
         for (int i = 0; i < 2 * N; i++) {
             for (int j = 0; j < i; j++) {
                 if (!disjoint(input[i], input[j])) { //i와 j가 겹치는 경우
+//                    System.out.println(i + " : " + input[i][0] + " ~ " + input[i][1]);
+//                    System.out.println(j + " : " + input[j][0] + " ~ " + input[j][1]);
                     graph.get(i * 2).add(j * 2 + 1); // i -> ~j
-                    graph.get(j * 2).add(i * 2 +1); // j -> ~i
+                    graph.get(j * 2).add(i * 2 + 1); // j -> ~i
                 }
             }
         }
+    }
+
+
+    private static int[] solve2SAT() {
+        int n = graph.size();
+
+        List<Integer> label = tarjanSCC();
+
+//        System.out.println("label : ");
+//        label.forEach((value) -> System.out.print(value + " "));
+        // check possible
+        for (int i = 0; i < 2 * N; i += 2) {
+            if (Objects.equals(label.get(i), label.get(i + 1))) {
+                return new int[0];
+            }
+        }
+
+        int[] value = new int[2 * N];
+        Arrays.fill(value, -1);
+
+        List<Pair> order = new ArrayList<>();
+        for (int i = 0; i < 4 * N; i++) {
+            order.add(new Pair(label.get(i) * -1, i));
+        }
+
+        order.sort(Comparator.comparing(Pair::getFirst));
+
+        for (int i = 0; i < 2 * N; i++) {
+            int vertex = order.get(i).second;
+            int variable = vertex / 2;
+            int isFalse = vertex % 2 == 0 ? 0 : 1;
+
+            if (value[variable] != -1) continue;
+
+            value[variable] = isFalse;
+        }
+        return value;
+
+    }
+
+    static int[] sccId;
+    static int[] discovered;
+    static Stack<Integer> st;
+    static int sccCounter, vertexCounter;
+
+    private static List<Integer> tarjanSCC() {
+        init();
+
+        for (int i = 0; i < graph.size(); i++) {
+            if (discovered[i] == -1) {
+                scc(i);
+            }
+        }
+        return Arrays.stream(sccId).boxed().collect(toList());
+    }
+
+    private static int scc(int here) {
+
+        vertexCounter += 1;
+        int ret = vertexCounter;
+        discovered[here] = vertexCounter;
+        st.push(here);
+
+        for (int i = 0; i < graph.get(here).size(); i++) {
+            int there = graph.get(here).get(i);
+
+            if (discovered[there] == -1) ret = Math.min(ret, scc(there));
+            else if (sccId[there] == -1) ret = Math.min(ret, discovered[there]);
+        }
+
+        if (ret == discovered[here]) {
+            while (true) {
+                int t = st.pop();
+                sccId[t] = sccCounter;
+                if (t == here) break;
+            }
+            sccCounter += 1;
+        }
+        return ret;
+    }
+
+    private static void init() {
+        st = new Stack<>();
+        sccId = new int[4 * N];
+        discovered = new int[4 * N];
+        Arrays.fill(discovered, -1);
+        Arrays.fill(sccId, -1);
     }
 
     private static boolean disjoint(int[] meeting1, int[] meeting2) {
@@ -163,7 +181,7 @@ public class MainMEETINGROOM_SongTaeheon {
 
         input = new int[2 * N][2];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < 2 * N; i += 2) {
             StringTokenizer st1 = new StringTokenizer(br.readLine());
             int start1 = Integer.parseInt(st1.nextToken());
             int end1 = Integer.parseInt(st1.nextToken());
