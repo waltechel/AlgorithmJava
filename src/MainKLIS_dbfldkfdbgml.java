826fdd8de15ec8e532474f0240d90e73fc9e2db1
knecht <ldj910505@naver.com>
2022-01-23 19:31:00 +0900

    code : KLIS 소스 수정했습니다
    감사합니다

--- src/MainKLIS_dbfldkfdbgml.java
+++ src/MainKLIS_dbfldkfdbgml.java
@@ -30,7 +30,7 @@ public class MainKLIS_dbfldkfdbgml {
 			lis = new int[N + 1];
 			count = new long[N + 1];
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
+			for (int i = 1; i <= N; i++) {
 				A[i] = Integer.parseInt(st.nextToken());
 			}
 			for (int i = 0; i <= N; i++) {
@@ -38,9 +38,9 @@ public class MainKLIS_dbfldkfdbgml {
 				count[i] = -1;
 			}
 
-			bw.write((lis(-1) - 1) + "\n");
+			bw.write((lis(0) - 1) + "\n");
 			ArrayList<Integer> result = new ArrayList<>();
-			reconstruct(-1, K - 1, result);
+			reconstruct(0, K - 1, result);
 			for (Integer e : result) {
 				bw.write(e + " ");
 			}
@@ -52,26 +52,17 @@ public class MainKLIS_dbfldkfdbgml {
 		br.close();
 	}
 
-	private static void reconstruct(int start, long skip, ArrayList<Integer> result) {
-		if (start != -1) {
-			result.add(A[start]);
+	private static void reconstruct(int startIndex, long skip, ArrayList<Integer> result) {
+		if (startIndex != 0) {
+			result.add(A[startIndex]);
 		}
 		ArrayList<int[]> followers = new ArrayList<>();
-		for (int next = start + 1; next < N; next++) {
-			if ((start == -1 || A[start] < A[next]) && lis(start) == lis(next) + 1) {
+		for (int next = startIndex + 1; next <= N; next++) {
+			if (A[startIndex] < A[next] && lis(startIndex) == lis(next) + 1) {
 				followers.add(new int[] { A[next], next });
 			}
 		}
-		Collections.sort(followers, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if (Integer.compare(o1[0], o2[0]) == 0) {
-					return Integer.compare(o1[1], o2[1]);
-				} else {
-					return Integer.compare(o1[0], o2[0]);
-				}
-			}
-		});
+		Collections.sort(followers, (o1, o2) -> Integer.compare(o1[0], o2[0]));
 		for (int i = 0; i < followers.size(); i++) {
 			int index = followers.get(i)[1];
 			long cnt = count(index);
@@ -87,31 +78,31 @@ public class MainKLIS_dbfldkfdbgml {
 
 	private static long count(int start) {
 		if (lis(start) == 1) {
-			return 1;
+			return count[start] = 1;
 		}
-		if (count[start + 1] != -1) {
-			return count[start + 1];
+		if (count[start] != -1) {
+			return count[start];
 		}
 		long ret = 0;
-		for (int next = start + 1; next < N; next++) {
-			if ((start == -1 || A[start] < A[next]) && lis(start) == lis(next) + 1) {
+		for (int next = start + 1; next <= N; next++) {
+			if (A[start] < A[next] && lis(start) == lis(next) + 1) {
 				ret = Math.min(Long.MAX_VALUE, ret + count(next));
 			}
 		}
-		return count[start + 1] = ret;
+		return count[start] = ret;
 	}
 
-	private static int lis(int start) {
-		if (lis[start + 1] != -1) {
-			return lis[start + 1];
+	private static int lis(int now) {
+		if (lis[now] != -1) {
+			return lis[now];
 		}
 		int ret = 1;
-		for (int next = start + 1; next < N; next++) {
-			if (start == -1 || A[start] < A[next]) {
+		for (int next = now + 1; next <= N; next++) {
+			if (A[now] < A[next]) {
 				ret = Math.max(ret, lis(next) + 1);
 			}
 		}
-		return lis[start + 1] = ret;
+		return lis[now] = ret;
 	}
 
 }
