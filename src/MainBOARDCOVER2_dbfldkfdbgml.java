9bf0c2a21e8b71ae8abcbccdbdbf8d852785b1ff
knecht <ldj910505@naver.com>
2022-03-13 21:02:22 +0900

    CODE : BOARDCOVER, MINASTIRITH 문제 풀었다
    - boardcover는 시간초과
    - minastirith는 답 보고 베낌

---
+++ src/MainBOARDCOVER2_dbfldkfdbgml.java
@@ -0,0 +1,228 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+import javax.sound.sampled.Line;
+
+/**
+ * 판 돌려서 기초적으로 구현
+ * 최대 판을 채우는 방법은 추가해봄
+ * 하지만 다음 사례에서 시간 초과가 남
+ * 1
+ * 10 10 1 1
+ * ..........
+ * ..........
+ * ..........
+ * ..........
+ * ..........
+ * ..........
+ * ..........
+ * ..........
+ * ..........
+ * ..........
+ * #
+ * 
+ * @author leedongjun
+ *
+ */
+public class MainBOARDCOVER2_dbfldkfdbgml {
+
+	private static int R, C, H, W;
+	private static boolean[][] checked, pad1, pad2, pad3, pad4;
+	private static int answer;
+	private static int maxCountForPruning;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int test = 0; test < T; test++) {
+			st = new StringTokenizer(br.readLine());
+			H = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			R = Integer.parseInt(st.nextToken());
+			C = Integer.parseInt(st.nextToken());
+
+			checked = new boolean[H][W];
+			for (int i = 0; i < H; i++) {
+				String line = br.readLine();
+				for (int j = 0; j < W; j++) {
+					char c = line.charAt(j);
+					if (c == '.') {
+						checked[i][j] = false;
+					} else {
+						checked[i][j] = true;
+					}
+				}
+			}
+
+			pad1 = new boolean[R][C];
+			for (int i = 0; i < R; i++) {
+				String line = br.readLine();
+				for (int j = 0; j < C; j++) {
+					char c = line.charAt(j);
+					if (c == '.') {
+						pad1[i][j] = false;
+					} else {
+						pad1[i][j] = true;
+					}
+				}
+			}
+
+			pad2 = rotate(pad1);
+			pad3 = rotate(pad2);
+			pad4 = rotate(pad3);
+
+			answer = 0;
+			maxCountForPruning = calculate();
+			for (int i = 0; i < H; i++) {
+				for (int j = 0; j < W; j++) {
+					if (canPruning(1)) {
+						continue;
+					}
+					if (isSafe(i, j, pad1)) {
+						dfs(i, j, pad1, 1);
+						clear(i, j, pad1);
+					}
+					if (isSafe(i, j, pad2)) {
+						dfs(i, j, pad2, 1);
+						clear(i, j, pad2);
+					}
+					if (isSafe(i, j, pad3)) {
+						dfs(i, j, pad3, 1);
+						clear(i, j, pad3);
+					}
+					if (isSafe(i, j, pad4)) {
+						dfs(i, j, pad4, 1);
+						clear(i, j, pad4);
+					}
+				}
+			}
+
+			bw.write(answer + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static boolean canPruning(int count) {
+		return maxCountForPruning < count;
+	}
+
+	private static int calculate() {
+		int a = 0;
+		for (int i = 0; i < checked.length; i++) {
+			for (int j = 0; j < checked.length; j++) {
+				if (checked[i][j] == false) {
+					a++;
+				}
+			}
+		}
+		int b = 0;
+		for (int i = 0; i < pad1.length; i++) {
+			for (int j = 0; j < pad1[i].length; j++) {
+				if (pad1[i][j]) {
+					b++;
+				}
+			}
+		}
+		return a / b;
+	}
+
+	private static void clear(int r, int c, boolean[][] pad) {
+		for (int i = 0; i < pad.length; i++) {
+			for (int j = 0; j < pad[i].length; j++) {
+				if (pad[i][j]) {
+					checked[r + i][c + j] = false;
+				}
+			}
+		}
+
+	}
+
+	private static void dfs(int r, int c, boolean[][] pad, int count) {
+		if (count >= answer) {
+			answer = count;
+		}
+
+		for (int i = 0; i < pad.length; i++) {
+			for (int j = 0; j < pad[i].length; j++) {
+				if (pad[i][j]) {
+					checked[r + i][c + j] = true;
+				}
+			}
+		}
+
+		for (int i = r; i < checked.length; i++) {
+			for (int j = 0; j < checked[i].length; j++) {
+				if (i == r && j <= c) {
+					continue;
+				}
+				if (canPruning(count + 1)) {
+					continue;
+				}
+				if (isSafe(i, j, pad1)) {
+					dfs(i, j, pad1, count + 1);
+					clear(i, j, pad1);
+				}
+				if (isSafe(i, j, pad2)) {
+					dfs(i, j, pad2, count + 1);
+					clear(i, j, pad2);
+				}
+				if (isSafe(i, j, pad3)) {
+					dfs(i, j, pad3, count + 1);
+					clear(i, j, pad3);
+				}
+				if (isSafe(i, j, pad4)) {
+					dfs(i, j, pad4, count + 1);
+					clear(i, j, pad4);
+				}
+			}
+		}
+
+	}
+
+	private static boolean isSafe(int r, int c, boolean[][] pad) {
+		if (r + pad.length - 1 >= checked.length) {
+			return false;
+		}
+		if (c + pad[0].length - 1 >= checked[0].length) {
+			return false;
+		}
+
+		for (int i = 0; i < pad.length; i++) {
+			for (int j = 0; j < pad[i].length; j++) {
+				if (checked[r + i][c + j] && pad[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private static void println(boolean[][] pad) {
+		for (int i = 0; i < pad.length; i++) {
+			System.out.println(Arrays.toString(pad[i]));
+		}
+		System.out.println();
+	}
+
+	private static boolean[][] rotate(boolean[][] pad) {
+		boolean[][] ret = new boolean[pad[0].length][pad.length];
+		for (int i = 0; i < ret.length; i++) {
+			for (int j = 0; j < ret[i].length; j++) {
+				ret[i][j] = pad[pad.length - 1 - j][i];
+			}
+		}
+		return ret;
+	}
+
+}
---
+++ src/MainMINASTIRITH_dbfldkfdbgml.java
@@ -0,0 +1,121 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/** 
+ * 원형 구간을 선형 구간으로 바꾸는 기법
+ * 라인 스위핑 추가
+ * @author leedongjun
+ *
+ */
+public class MainMINASTIRITH_dbfldkfdbgml {
+
+	private static final double PI = Math.PI;
+	private static final int MAX = Integer.MAX_VALUE / 10;
+	private static double[] y, x, r;
+	private static int N;
+	private static Pair[] ranges;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int test = 0; test < T; test++) {
+
+			N = Integer.parseInt(br.readLine());
+			y = new double[N];
+			x = new double[N];
+			r = new double[N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				y[i] = Double.parseDouble(st.nextToken());
+				x[i] = Double.parseDouble(st.nextToken());
+				r[i] = Double.parseDouble(st.nextToken());
+			}
+
+			convertToRange();
+
+			int ret = solve();
+
+			if (ret != MAX) {
+				bw.write(ret + "\n");
+			} else {
+				bw.write("IMPOSSIBLE\n");
+			}
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static int solve() {
+		int ret = MAX;
+		for (int i = 0; i < N; i++) {
+			if (ranges[i].first <= 0 || ranges[i].second >= 2 * PI) {
+				// i 번째 항목을 0 을 포함하는 값으로 선정하는 경우
+				double lo = fmod(ranges[i].second, 2 * PI);
+				double hi = fmod(ranges[i].first + 2 * PI, 2 * PI);
+				ret = Math.min(ret, 1 + linear(lo, hi));
+			}
+		}
+		return ret;
+	}
+
+	private static int linear(double begin, double end) {
+		int used = 0, idx = 0;
+		// 덮지 못한 선분이 남아 있는 동안 계속한다.
+		while (begin < end) {
+			// begin보다 이전에 시작하는 구간 중 가장 늦게 끝나는 구간을 찾는다.
+			double maxCover = -1;
+			while (idx < N && ranges[idx].first <= begin) {
+				maxCover = Math.max(maxCover, ranges[idx++].second);
+			}
+
+			// 선분을 덮지 못하게 될 떄
+			if (maxCover <= begin) {
+				return MAX;
+			} else {
+				// 왜 이거 used를 줘야 하는지는 잘 모르겠습니다
+				begin = maxCover;
+				used++;
+			}
+
+		}
+
+		return used;
+	}
+
+	private static void convertToRange() {
+		ranges = new Pair[N];
+		for (int i = 0; i < N; i++) {
+			double loc = fmod(2 * PI + Math.atan2(y[i], x[i]), 2 * PI);
+			double range = 2.0f * Math.asin(r[i] / 2.0 / 8.0);
+			ranges[i] = new Pair(loc - range, loc + range);
+		}
+		Arrays.sort(ranges, (o1, o2) -> Double.compare(o1.first, o2.first));
+	}
+
+	private static double fmod(double a, double b) {
+		int result = (int) Math.floor(a / b);
+		return a - result * b;
+	}
+
+	private static class Pair {
+		double first;
+		double second;
+
+		public Pair(double first, double second) {
+			super();
+			this.first = first;
+			this.second = second;
+		}
+
+	}
+
+}
