6de58568b0ed377ea90811ff38708dc58e42605b
knecht <ldj910505@naver.com>
2022-06-26 21:34:30 +0900

    B, C 문제까지도 해봤다.

--- src/Main1647C_dbfldkfdbgml.java
+++ src/Main1647C_dbfldkfdbgml.java
@@ -2,18 +2,13 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
  * @author leedongjun
- *         It is also clear that if there are 3 such cells,
- *         then there will be two intersecting rectangles.
- *         Therefore, you just need to check if there is a
- *         2×2 square in which there are exactly 3 colored
- *         cells.
  */
-public class Main1647B_dbfldkfdbgml {
+public class Main1647C_dbfldkfdbgml {
 
 	public static void main(String[] args) throws Exception {
 
@@ -23,8 +18,8 @@ public class Main1647B_dbfldkfdbgml {
 
 		int T = Integer.parseInt(br.readLine());
 		for (int test = 0; test < T; test++) {
-			st = new StringTokenizer(br.readLine());
 			int N, M;
+			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			String[] map = new String[N];
@@ -32,17 +27,29 @@ public class Main1647B_dbfldkfdbgml {
 				map[i] = br.readLine();
 			}
 
-			boolean flag = false;
-			for (int i = 0; i < N - 1; i++) {
-				for (int j = 0; j < M - 1; j++) {
-					int cnt = map[i].charAt(j) + map[i].charAt(j + 1) + map[i + 1].charAt(j) + map[i + 1].charAt(j + 1) - '0' * 4;
-					if (cnt == 3) {
-						flag = true;
+			ArrayList<int[]> answer = new ArrayList<>();
+			if (map[0].charAt(0) == '1') {
+				bw.write("-1\n");
+				continue;
+			}
+
+			for (int j = M - 1; j >= 0; j--) {
+				for (int i = N - 1; i >= 0; i--) {
+					if (map[i].charAt(j) == '1') {
+						if (i != 0) {
+							answer.add(new int[] { i, j + 1, i + 1, j + 1 });
+						} else {
+							answer.add(new int[] { i + 1, j, i + 1, j + 1 });
+						}
 					}
 				}
 			}
 
-			bw.write(flag ? "NO\n" : "YES\n");
+			bw.write(answer.size() + "\n");
+			for (int[] element : answer) {
+				bw.write(element[0] + " " + element[1] + " " + element[2] + " "
+						+ element[3] + "\n");
+			}
 		}
 
 		bw.flush();
